341c8a980bd63fd4cc6476518d1331e7b22d14eb
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-09-14 23:06:47 +0530

    update the js

--- 05-DOM/02-DOM-Relations/23-domRelations.js
+++ 05-DOM/02-DOM-Relations/23-domRelations.js
@@ -6,7 +6,7 @@ const parent = document.querySelector('.parent')
     //    console.log(parent.children[1].innerHTML);
 //****************************************** */
         // for (let i = 0; i < parent.children.length; i++) {
-        //     // console.log(parent.children[i].innerHTML);
+        //      console.log(parent.children[i].innerHTML);
             
         // }
  //******************************************* */       
@@ -17,7 +17,7 @@ const parent = document.querySelector('.parent')
     //   console.log(parent.lastElementChild); // for last child element
 
       const dayOne = document.querySelector('.day')
-       //console.log(dayOne);
+    //    console.log(dayOne);
         //console.log(dayOne.parentElement);
         // console.log(dayOne.nextElementSibling);
 
